f3e064b3dc52fdeb70be69c674c5e1b44fa5e561
mu-777 <ryosuke.murata.7@gmail.com>
2015-09-02 18:31:27 +0900

    update it for UDP comm

--- relative_pose_publisher/src/relative_pose_publisher.py
+++ relative_pose_publisher/src/relative_pose_publisher.py
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import rospy
+import socket
+
 from kaken_msgs.msg import RelativePoseStamped
 from std_msgs.msg import Header
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Pose
 
 DEFAULT_NODE_NAME = 'camera_node'
+IP_ADDRESS = '127.0.0.1'
+PORT = 8888
+BUFFER_SIZE = 1024
 
 
 class RelativePosePublisher(object):
@@ -16,9 +21,11 @@ class RelativePosePublisher(object):
                                                queue_size=5)
         self._kohga3_1_pose = RelativePoseStamped(header=Header(stamp=rospy.Time.now()),
                                                   target='kohga3_1')
+        self._clientsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def activate(self):
         rospy.Subscriber('kohga3_1_twist', Twist, self._twist_callback)
+        self._clientsock.bind((IP_ADDRESS, PORT))
         return self
 
     def _twist_callback(self, twist):
@@ -28,7 +35,13 @@ class RelativePosePublisher(object):
         self._kohga3_1_pose.pose.position.z += twist.linear.z
 
     def update(self):
+        recv_str, addr = self._clientsock.recvfrom(BUFFER_SIZE)
+        self._kohga3_1_pose.header.stamp = rospy.Time.now()
+        self._kohga3_1_pose.pose = self._unpack_recv_str(recv_str)
         return self
+
+    def _unpack_recv_str(self, str):
+        return Pose()
 
     def publish_data(self):
         self._pose_publisher.publish(self._kohga3_1_pose)
