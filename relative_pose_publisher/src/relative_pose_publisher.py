b44f9c191e33cb8826125fb7611f4cf0293a0776
mu-777 <ryosuke.murata.7@gmail.com>
2015-09-04 20:57:03 +0900

    change udp to tcpip

--- relative_pose_publisher/src/relative_pose_publisher.py
+++ relative_pose_publisher/src/relative_pose_publisher.py
@@ -3,14 +3,15 @@
 
 import rospy
 import socket
+import re
 
 from kaken_msgs.msg import RelativePoseStamped
 from std_msgs.msg import Header
-from geometry_msgs.msg import Twist, Pose
+from geometry_msgs.msg import Twist, Pose, Vector3
 
 DEFAULT_NODE_NAME = 'camera_node'
-IP_ADDRESS = '192.168.0.3'
-PORT = 5555
+IP_ADDRESS = '192.168.0.12'
+PORT = 55555
 BUFFER_SIZE = 1024
 
 
@@ -21,9 +22,10 @@ class RelativePosePublisher(object):
                                                queue_size=5)
         self._kohga3_1_pose = RelativePoseStamped(header=Header(stamp=rospy.Time.now()),
                                                   target='kohga3_1')
-        self._clientsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self._clientsock.bind((IP_ADDRESS, PORT))
-
+        self._clientsock = socket.socket(socket.AF_INET,
+                                         socket.SOCK_STREAM)
+        self._clientsock.connect((IP_ADDRESS, PORT))
+        self._splitter = re.compile(r"[\n,]")
 
     def activate(self):
         rospy.Subscriber('kohga3_1_twist', Twist, self._twist_callback)
@@ -37,14 +39,23 @@ class RelativePosePublisher(object):
         self._kohga3_1_pose.pose.position.z += twist.linear.z
 
     def update(self):
-        recv_str, addr = self._clientsock.recv(BUFFER_SIZE)
-        print(recv_str)
-        # self._kohga3_1_pose.header.stamp = rospy.Time.now()
-        # self._kohga3_1_pose.pose = self._unpack_recv_str(recv_str)
+        recv_str = self._clientsock.recv(BUFFER_SIZE)
+        if self._check_valid_data(recv_str):
+            id, pose = self._unpack_recv_data(recv_str)
+            self._kohga3_1_pose.header.stamp = rospy.Time.now()
+            self._kohga3_1_pose.pose = pose
         return self
 
-    def _unpack_recv_str(self, str):
-        return Pose()
+    def _check_valid_data(self, recv_str):
+        return recv_str[0] == 'P'
+
+    def _unpack_recv_data(self, recv_str):
+        recv_list = self._splitter.split(recv_str)
+        print(recv_list)
+        id = recv_list[0]
+        return id, Pose(position=Vector3(x=float(recv_list[2]),
+                                         y=float(recv_list[3]),
+                                         z=float(recv_list[1])))
 
     def publish_data(self):
         self._pose_publisher.publish(self._kohga3_1_pose)
